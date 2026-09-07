b4b334692060c501ba33fce6a6bbb446f970e6e4
David Priest <david.priest898@gmail.com>
2026-09-07 11:55:54 +0900

    Sync the GateLab core to 4375b3f
    GateLab-dev master at 4375b3f: Colour by a categorical colData column,
    on top of the memberships and pooled-label changes. Built with
    npm run build:embed; CORE_PROVENANCE.json regenerated.

--- inst/react-app/index.es-z2wcfiWG.js
+++ inst/react-app/index.es-z2wcfiWG.js
@@ -1,5 +1,5 @@
-import { q as Va, t as il } from "./embed-CPEYZs4l.js";
-import { _ as La } from "./jspdf.es.min-DWroBZeZ.js";
+import { q as Va, t as il } from "./embed-CesrH4uk.js";
+import { _ as La } from "./jspdf.es.min-DyyjjHxp.js";
 var fn = {}, cn = {}, cr, vn;
 function Q() {
   if (vn) return cr;
